b2d37c63ec87aea97409ac1c5bc6adbc48e9ad5e
gmh1627 <3558965271@qq.com>
2024-04-13 20:04:31 +0800

    commit

--- lab2/testjson.py
+++ lab2/testjson.py
@@ -11,9 +11,17 @@ def find_json_differences(file1, file2):
     for line in diff:
         if line.startswith('+') or line.startswith('-'):
             print(line)
+    """
+    +表示该行存在于file2中，但不存在于file1中。
+    -表示该行存在于file1中，但不存在于file2中。
+    空格表示该行在两个文本序列中是相同的。
+    ?表示行存在于两个文本序列中，但具体差异不同于上述三种情况，通常表示不可打印的字符等。
+    """
 
 # 指定要比较的两个 JSON 文件路径
 file1 = 'researchers.json'
-file2 = 'test.json'
+file2 = r'F:\Desktop\researchers.json'
+
+find_json_differences(file1, file2)
 
 find_json_differences(file1, file2)
