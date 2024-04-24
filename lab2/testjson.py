b9f2c4978c348a94352db221167b26bcdd3fcadf
gmh1627 <3558965271@qq.com>
2024-04-24 16:10:44 +0800

    commit

--- lab2/testjson.py
+++ lab2/testjson.py
@@ -9,19 +9,15 @@ def find_json_differences(file1, file2):
     diff = list(difflib.ndiff(json.dumps(data1, indent=2).splitlines(), json.dumps(data2, indent=2).splitlines()))
 
     for line in diff:
-        if line.startswith('+') or line.startswith('-'):
-            print(line)
+        print(line)
     """
     +表示该行存在于file2中，但不存在于file1中。
     -表示该行存在于file1中，但不存在于file2中。
-    空格表示该行在两个文本序列中是相同的。
-    ?表示行存在于两个文本序列中，但具体差异不同于上述三种情况，通常表示不可打印的字符等。
+    ?表示两个文件在这一行有微小的差异，例如空格或者标点符号的不同。
     """
 
 # 指定要比较的两个 JSON 文件路径
 file1 = 'researchers.json'
-file2 = r'F:\Desktop\researchers.json'
-
-find_json_differences(file1, file2)
-
+file2 = 'researchers(1).json'
+#file2 = r'F:\Desktop\researchers.json'，r表示原始字符串，不会对字符串中的特殊字符进行转义
 find_json_differences(file1, file2)
